acd52d73ffe4cc95b71a7fc51a26d19d6fa00b61
howardch <howardch@outlook.com>
2022-07-16 20:28:20 +0800

    code refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "../node_modules/antd/dist/antd.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
