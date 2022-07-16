55d17b1bb6e2be9487af9c5379c2728bd4d0ee9d
howardch <howardch@outlook.com>
2022-07-16 22:34:19 +0800

    add shuffle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { loadWords } from './shared/resources'
 
 import WordCard from './components/WordCard';
 import { Button, Checkbox } from 'antd';
+import _ from 'lodash';
 
 function App() {
   const [words, setWords] = useState([]);
@@ -11,6 +12,7 @@ function App() {
   const [current, setCurrent] = useState(0);
   const [answerBlured, setAnswerBlured] = useState(true);
   const [swapAnswer, setSwapAnswer] = useState(false);
+  const [shuffled, setShuffled] = useState(false);
 
   useEffect(() => {
     loadWords().then(d => {
@@ -40,20 +42,34 @@ function App() {
     setSwapAnswer(e.target.checked);
   }, []);
 
+  let onShuffledChanged = useCallback(e => {
+    setShuffled(e.target.checked);
+    if (e.target.checked) {
+      let shuffledWords = _.shuffle(words);
+      setWords(shuffledWords);
+    } else {
+      loadWords().then(d => {
+        setWords(d);
+        setWordCount(d.length);
+      });
+    }
+  }, [words]);
+
   return (
     <div className="App">
       <div style={{ 'flexGrow': 1 }}>
         <WordCard answerBlured={answerBlured} swapAnswer={swapAnswer} {...words[current]}></WordCard>
       </div>
       <div>
-        <Button type="normal" disabled={!(current > 0)} onClick={onPreviousWord}>Previous</Button>
+        <Button size="large" type="normal" disabled={!(current > 0)} onClick={onPreviousWord}>Previous</Button>
         <span style={{ display: "inline-block", width: 60 }}>{current + 1}/{wordCount}</span>
-        <Button type="primary" disabled={!(current < wordCount - 1)} onClick={onNextWord}>Next</Button>
+        <Button size="large" type="primary" disabled={!(current < wordCount - 1)} onClick={onNextWord}>Next</Button>
       </div>
       <div className='settings'>
         <div style={{ width: 120 }}>
           <div><Checkbox defaultChecked={answerBlured} onChange={onAnswerBluredChanged}>Blur Answer</Checkbox></div>
           <div><Checkbox defaultChecked={swapAnswer} onChange={onSwapAnswerChanged}>Swap Q&A</Checkbox></div>
+          <div><Checkbox defaultChecked={shuffled} onChange={onShuffledChanged}>Shuffle</Checkbox></div>
         </div>
       </div>
     </div>
