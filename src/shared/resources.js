097ebcd654633c0b84d0de651c907cf80ac5a8ac
howardch <howardch@outlook.com>
2022-07-16 21:08:42 +0800

    resource refactor

--- src/shared/resources.js
+++ src/shared/resources.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import csv from 'csvtojson'
 
 async function loadResources() {
-    const res = await axios.get('./assets/index.json');
+    const res = await axios.get('./assets/english/words/index.json');
 
     const result = {};
     for (let path of res.data) {
-        const resWords = await axios.get(`./assets/${path}`);
+        const resWords = await axios.get(`./assets/english/words/${path}`);
         const csvWords = resWords.data;
         const content = await csv().fromString(csvWords);
         result[path.replace(/\.csv/ig, '')] = content;
