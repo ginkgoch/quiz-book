b8393043fb14e6d7d9e116c8050d0cd7ac81de35
ginkgoch <ginkgoch@outlook.com>
2022-10-03 16:26:05 +0800

    change a router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import EnglishQuizPage from './pages/EnglishQuizPage';
 
