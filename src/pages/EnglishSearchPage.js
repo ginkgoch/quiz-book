c6dfe17a45b503b292eb756b87aa2ad42899506c
ginkgoch <ginkgoch@outlook.com>
2022-10-05 17:36:28 +0800

    add search func basis

--- src/pages/EnglishSearchPage.js
+++ src/pages/EnglishSearchPage.js
@@ -1,9 +1,45 @@
+import { AutoComplete } from 'antd';
+import { useState, useEffect, useCallback } from 'react';
 import BackToHomeButton from '../components/BackToHomeButton';
+import { loadWords } from '../shared/resources';
+
+function _renderOption(word) {
+    return { value: word['english'], label: <EnglishSearchOption word={word}></EnglishSearchOption> };
+}
+
+function EnglishSearchOption({ word }) {
+    return `${word.english} /${word.chinese}/`;
+}
 
 function EnglishSearchPage() {
+    const [options, setOptions] = useState([]);
+    const [words, setWords] = useState([]);
+    const [selectedWord, setSelectedWord] = useState(null);
+
+    const onSearch = useCallback(searchText => {
+        const searchingWords = words.filter(w => w.english.startsWith(searchText)).map(_renderOption);
+        setOptions(searchingWords);
+    }, [words]);
+
+    const onSelect = useCallback(data => {}, [
+
+    ]);
+
+    useEffect(() => {
+        loadWords('english', 'words').then(d => {
+            setWords(d);
+        });
+    }, []);
+
     return <>
         <BackToHomeButton></BackToHomeButton>
-        <div>Search Page</div>
+        <div>
+            <AutoComplete
+                options={options}
+                onSearch={onSearch}
+                style={{ width: 200 }}
+                placeholder="Input searching text"></AutoComplete>
+        </div>
     </>
 }
 
